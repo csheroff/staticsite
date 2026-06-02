c1de1f76531e34fb7d6efde37e97fadce79bab7b
Tobitantin <csheroff@comcast.net>
2026-06-02 10:38:49 -0400

    enum 2

--- src/textnode.py
+++ src/textnode.py
@@ -1,5 +1,6 @@
 from enum import Enum
 
+
 class TextType(Enum):
     TEXT = "text"
     BOLD = "bold"
@@ -7,6 +8,7 @@ class TextType(Enum):
     CODE = "code"
     LINK = "link"
     IMAGE = "image"
+
 
 class TextNode:
     def __init__(self, text: str, text_type: TextType, url: str | None = None) -> None:
